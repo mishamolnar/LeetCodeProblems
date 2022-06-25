62710734172e5a905fe28af75546c62f39022bb2
mmolnar <mmolnar@atlassian.com>
2022-06-25 13:05:05 +0300

    25/31

--- matrixes/AndroidUnlockPatterns.java
+++ matrixes/AndroidUnlockPatterns.java
@@ -1,5 +1,34 @@
 package LeetCode.matrixes;
 
 public class AndroidUnlockPatterns {
+    public int numberOfPatterns(int m, int n) {
+        int[][] skip = new int[10][10];
+        skip[1][3] = skip[3][1] = 2;
+        skip[1][7] = skip[7][1] = 4; //між 7 і 1 стоїть 4, це ми і вписуєио в масив
+        skip[7][9] = skip[9][7] = 8;
+        skip[3][9] = skip[9][3] = 6;
+        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
+        boolean[] visited = new boolean[10];
+        int res = 0;
+        for (int i = m; i <= n; i++) {
+            res += dfs(visited, skip, 1, i - 1) * 4; //суть в тому що ми можемо це оптимізувати запустивши почсаток на одному і потім просто помножити на 4
+            res += dfs(visited, skip, 2, i - 1) * 4;
+            res += dfs(visited, skip, 5, i - 1);
+        }
+        return res;
+    }
 
+    private int dfs(boolean[] visited, int[][] skip, int cur, int remain) {
+        if (remain < 0) return 0;
+        if (remain == 0) return 1;
+        visited[cur] = true;
+        int res = 0;
+        for (int i = 1; i <= 9; i++) {
+            if (!visited[i] && (skip[cur][i] == 0 || (visited[skip[cur][i]]))) {
+                res += dfs(visited, skip, i, remain - 1);
+            }
+        }
+        visited[cur] = false;
+        return res;
+    }
 }
