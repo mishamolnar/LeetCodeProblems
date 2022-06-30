faaf34ac881f132fd3e4eab0b3c5ebd4db3a7f54
mmolnar <mmolnar@atlassian.com>
2022-06-30 18:35:22 +0300

    29/31

--- Intervals/InsertInterval.java
+++ Intervals/InsertInterval.java
@@ -8,6 +8,24 @@ import java.util.List;
 // time and space complexity - O(n)
 public class InsertInterval {
 
+
+    public int[][] insertTwo(int[][] intervals, int[] newInterval) {
+        if (intervals.length == 0) return new int[][]{newInterval};
+        int[][] res = new int[intervals.length + 1][2];
+        int pointer = 0, sourcePointer = 0;
+        while (sourcePointer < intervals.length && intervals[sourcePointer][1] < newInterval[0]) //шукаємо перший відрізок який пересікається з таргет відрізком
+            res[pointer++] = intervals[sourcePointer++];
+        while (sourcePointer < intervals.length && intervals[sourcePointer][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[sourcePointer][0]); //ітеруємось доки відрізок пересікається з таргет відрізком
+            newInterval[1] = Math.max(newInterval[1], intervals[sourcePointer++][1]);
+        }
+        res[pointer++] = newInterval; //додаємо таргет відрізок
+        while (sourcePointer < intervals.length) res[pointer++] = intervals[sourcePointer++]; //потім додаємо всі ініші відрізки
+        int[][] ans = new int[pointer][];
+        System.arraycopy(res, 0, ans, 0, ans.length);
+        return ans;
+    }
+
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int i = 0;
         int[][] result = new int[intervals.length + 1][];
@@ -36,7 +54,7 @@ public class InsertInterval {
 
     public static void main(String[] args) {
         InsertInterval insertInterval = new InsertInterval();
-        int[][] result = insertInterval.insert(new int[][]{{1,5}}, new int[]{6,8});
+        int[][] result = insertInterval.insertTwo(new int[][]{{1,2},{3,5},{6,7},{8,10},{12,16}}, new int[]{4,8});
         System.out.println(Arrays.deepToString(result));
     }
 
