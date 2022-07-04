0b34c63a1059e3039a063f51ca0d62d28331ec07
mmolnar <mmolnar@atlassian.com>
2022-07-04 19:58:30 +0300

    4/31

--- tree/ConstructTreeFromTreInOrder.java
+++ tree/ConstructTreeFromTreInOrder.java
@@ -13,6 +13,21 @@ public class ConstructTreeFromTreInOrder {
         System.out.println("DONE");
     }
 
+    public TreeNode buildTreeTwo(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
+        return buildTreeInBoundary(preorder, 0, inorder.length - 1, 0, map);
+    }
+
+    private TreeNode buildTreeInBoundary(int[] preorder, int inStart, int inEnd, int rootIndexPre, HashMap<Integer, Integer> inorder) {
+        if (inStart > inEnd) return null;
+        int rootVal = preorder[rootIndexPre];
+        int rootIndexIn = inorder.get(rootVal);
+        TreeNode left = buildTreeInBoundary(preorder, inStart, rootIndexIn - 1, rootIndexPre + 1, inorder);
+        TreeNode right = buildTreeInBoundary(preorder, rootIndexIn + 1, inEnd, rootIndexPre + rootIndexIn - inStart + 1, inorder);
+        return new TreeNode(rootVal, left, right);
+    }
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         HashMap<Integer, Integer> in = new HashMap<>();
         for (int i = 0; i < preorder.length; i++) {
