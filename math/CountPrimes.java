1a594f4a18c82281d0b718408e8cba88a1086422
mmolnar <mmolnar@atlassian.com>
2022-08-11 09:18:41 +0300

    11/31

---
+++ math/CountPrimes.java
@@ -0,0 +1,25 @@
+package LeetCode.math;
+
+//https://leetcode.com/problems/count-primes/
+public class CountPrimes {
+
+    //O(n * (log (log n))
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
+}
---
+++ matrixes/Search2DMatrixII.java
@@ -0,0 +1,13 @@
+package LeetCode.matrixes;
+
+public class Search2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+}
