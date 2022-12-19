b64f46354b4f6725687e4d6ead91a5a7a209b3de
mmo <michaellmuller423@gmail.com>
2022-12-19 09:35:53 +0200

    16.12

---
+++ backTracking/CrackTheSafe.java
@@ -0,0 +1,11 @@
+package LeetCode.backTracking;
+
+import java.util.Collections;
+
+public class CrackTheSafe {
+    public static void main(String[] args) {
+        System.out.println(String.join("", Collections.nCopies(3, "0")));
+    }
+
+
+}
