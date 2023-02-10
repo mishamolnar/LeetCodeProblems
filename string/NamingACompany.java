327e218b4b98905e835d3f20fb8971ac5fd365fe
mykhailo-molnar-imtf <Mykhailo.Molnar@imtf.com>
2023-02-10 17:36:37 +0200

    10.02

---
+++ string/NamingACompany.java
@@ -0,0 +1,27 @@
+package LeetCode.string;
+
+import java.util.HashSet;
+
+public class NamingACompany {
+    public long distinctNames(String[] ideas) {
+        HashSet<String>[] groups = new HashSet<>[26];
+        for (String idea : ideas) {
+            groups[idea.charAt(0) - 'a'].add(idea.substring(1));
+        }
+
+        int res = 0;
+        for (int i = 0; i < 26; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                int numberOfCommonSuffixes = 0;
+
+                for (String suffix : groups[i]) {
+                    if (groups[j].contains(suffix)) {
+                        numberOfCommonSuffixes++;
+                    }
+                }
+                res += (groups[i].size() - numberOfCommonSuffixes) * (groups[j].size() - numberOfCommonSuffixes) * 2;
+            }
+        }
+        return res;
+    }
+}
