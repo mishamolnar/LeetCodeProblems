4493106e4cdbae52ae4d76c88ab1aed28a8fd805
mykhailo-molnar-imtf <Mykhailo.Molnar@imtf.com>
2023-02-22 08:35:24 +0200

    14.02

--- binarySearch/GenericBinarySearch.java
+++ binarySearch/GenericBinarySearch.java
@@ -1,17 +1,19 @@
 package LeetCode.binarySearch;
 
+import java.util.function.Predicate;
+
 public class GenericBinarySearch {
 
     public static void main(String[] args) {
-        System.out.println(new GenericBinarySearch().leftMost(new int[]{1, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8}, 1));
+        System.out.println(new GenericBinarySearch().rightMost(new int[]{1, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8}, ((a) -> a > 1)));
     }
 
-    private int leftMost(int[] arr, int target) {
+    private int leftMost(int[] arr, Predicate<Integer> pre) {
         int left = -1; //excluded
         int right = arr.length; //answer
         while (right - left > 1) {
             int middle = left + (right - left) / 2;
-            if (arr[middle] >= target) {
+            if (pre.test(arr[middle])) {
                 right = middle;
             } else {
                 left = middle;
@@ -21,15 +23,15 @@ public class GenericBinarySearch {
     }
 
 
-    private int rightMost(int[] arr, int target) {
+    private int rightMost(int[] arr, Predicate<Integer> pre) {
         int left = -1; //answer
         int right = arr.length; //excluded
         while (right - left > 1) {
             int middle = left + (right - left) / 2;
-            if (arr[middle] <= target) {
-                left = middle;
-            } else {
+            if (pre.test(arr[middle])) {
                 right = middle;
+            } else {
+                left = middle;
             }
         }
         return left;
