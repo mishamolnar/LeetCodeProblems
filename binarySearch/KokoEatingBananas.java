d063fa619af395af54a3250426f51af11c555c0b
mykhailo-molnar-imtf <Mykhailo.Molnar@imtf.com>
2023-02-24 08:55:26 +0200

    23.02

--- binarySearch/KokoEatingBananas.java
+++ binarySearch/KokoEatingBananas.java
@@ -22,4 +22,27 @@ public class KokoEatingBananas {
         System.out.println(kokoEatingBananas.minEatingSpeed(new int[]{805306368,805306368,805306368},
         1000000000));
     }
+
+
+
+    public int minEatingSpeedTwo(int[] piles, int h) {
+        int left = 0, right = Arrays.stream(piles).max().getAsInt();
+        while (right - left > 1) {
+            int middle = left + (right - left) / 2;
+            if (test(piles, h, middle)) {
+                right = middle;
+            } else {
+                left = middle;
+            }
+        }
+        return right;
+    }
+
+    private boolean test(int[] piles, int h, int speed) {
+        for (int pile : piles) {
+            h -= pile / speed;
+            h -= pile % speed > 0 ? 1 : 0;
+        }
+        return h >= 0;
+    }
 }
