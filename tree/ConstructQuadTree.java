7689c95eefbf4cb1d62b471a23792547d2751260
mykhailo-molnar-imtf <Mykhailo.Molnar@imtf.com>
2023-02-27 10:47:29 +0200

    27.02

--- tree/ConstructQuadTree.java
+++ tree/ConstructQuadTree.java
@@ -3,34 +3,59 @@ package LeetCode.tree;
 //https://leetcode.com/problems/construct-quad-tree/submissions/
 public class ConstructQuadTree {
 
+//    public Node construct(int[][] grid) {
+//        return construct(grid, 0, 0, grid.length - 1, grid.length - 1);
+//    }
+
+//    private Node construct(int[][] grid, int left, int top, int bottom, int right) {
+//        if (bottom == top && left == right)
+//            return new Node(grid[top][left] == 1, true);
+//        int midHeight = top + (bottom - top) / 2;
+//        int midWidth = left + (right - left) / 2;
+//        Node topLeft = construct(grid, left, top, midHeight, midWidth);
+//        Node topRight = construct(grid, midWidth + 1, top, midHeight, right);
+//        Node bottomLeft = construct(grid, left, midHeight + 1, bottom, midWidth);
+//        Node bottomRight = construct(grid, midWidth + 1, midHeight + 1, bottom, right);
+//        if ((topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf) && ((topLeft.val && topRight.val && bottomLeft.val && bottomRight.val) || (!topLeft.val && !topRight.val && !bottomLeft.val && !bottomRight.val))) {
+//            return new Node(topLeft.val, true);
+//        } else return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+//    }
+
+    public static void main(String[] args) {
+        ConstructQuadTree constructQuadTree = new ConstructQuadTree();
+        System.out.println(constructQuadTree.construct(new int[][]{
+                {1,1,1,1},
+                {1,1,1,1},
+                {0,0,0,0},
+                {0,0,0,0}}));
+    }
+
+
+
+
     public Node construct(int[][] grid) {
         return construct(grid, 0, 0, grid.length - 1, grid.length - 1);
     }
 
-    private Node construct(int[][] grid, int left, int top, int bottom, int right) {
-        if (bottom == top && left == right)
-            return new Node(grid[top][left] == 1, true);
-        int midHeight = top + (bottom - top) / 2;
-        int midWidth = left + (right - left) / 2;
-        Node topLeft = construct(grid, left, top, midHeight, midWidth);
-        Node topRight = construct(grid, midWidth + 1, top, midHeight, right);
-        Node bottomLeft = construct(grid, left, midHeight + 1, bottom, midWidth);
-        Node bottomRight = construct(grid, midWidth + 1, midHeight + 1, bottom, right);
-        if ((topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf) && ((topLeft.val && topRight.val && bottomLeft.val && bottomRight.val) || (!topLeft.val && !topRight.val && !bottomLeft.val && !bottomRight.val))) {
-            return new Node(topLeft.val, true);
-        } else return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
-    }
-
-    public static void main(String[] args) {
-        ConstructQuadTree constructQuadTree = new ConstructQuadTree();
-        System.out.println(constructQuadTree.construct(new int[][]{{1,1,1,1,0,0,0,0},
-                {1,1,1,1,0,0,0,0},
-                {1,1,1,1,1,1,1,1},
-                {1,1,1,1,1,1,1,1},
-                {1,1,1,1,0,0,0,0},
-                {1,1,1,1,0,0,0,0},
-                {1,1,1,1,0,0,0,0},
-                {1,1,1,1,0,0,0,0}}));
+    //included everyting
+    private Node construct(int[][] grid, int left, int up, int right, int down) {
+        if (left == right && up == down) {
+            return new Node(grid[left][up] == 1, true);
+        }
+        int centerLeft = left + (right - left) / 2;
+        int centerRight = centerLeft + 1;
+        int centerUp = up + (down - up) / 2;
+        int centerDown = centerUp + 1;
+        Node leftUp = construct(grid, left, up, centerLeft, centerUp);
+        Node rightUp = construct(grid, centerRight, up, right, centerUp);
+        Node leftDown = construct(grid, left, centerDown, centerLeft, down);
+        Node rightDown = construct(grid, centerRight, centerDown, right, down);
+        if (leftUp.isLeaf && rightUp.isLeaf && leftDown.isLeaf && rightDown.isLeaf &&
+                (leftUp.val == rightUp.val && rightUp.val == leftDown.val && leftDown.val == rightDown.val)) {
+            return new Node(leftUp.val, true);
+        } else {
+            return new Node(true, false, leftUp, rightUp, leftDown, rightDown);
+        }
     }
 
     private class Node {
