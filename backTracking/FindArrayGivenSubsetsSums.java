58a768feb23d10130baf6b14ab734b883cca76e5
mishamolnar <michaellmuller423@gmail.com>
2023-03-28 12:28:53 +0300

    27.03

--- backTracking/FindArrayGivenSubsetsSums.java
+++ backTracking/FindArrayGivenSubsetsSums.java
@@ -1,10 +1,54 @@
 package LeetCode.backTracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+//https://leetcode.com/problems/find-array-given-subset-sums/description/
 public class FindArrayGivenSubsetsSums {
 
+
+    public int[] recoverArray(int n, int[] sums) {
+        Arrays.sort(sums);
+        if (sums.length == 2) {
+            return new int[]{sums[0] == 0 ? sums[1] : sums[0]};
+        }
+        int[] res = new int[getSize(sums.length)];
+        int[] left = new int[sums.length / 2];
+        int[] right = new int[sums.length / 2];
+        int diff = sums[1] - sums[0], leftCounter = 0, rightCounter = 0;
+        Map<Integer, Integer> set = new HashMap<>();
+        for (int sum : sums) {
+            if (set.getOrDefault(sum - diff, 0) > 0) {
+                left[leftCounter++] = sum - diff;
+                set.put(sum - diff, set.get(sum - diff) - 1);
+                right[rightCounter++] = sum;
+            } else set.put(sum, set.getOrDefault(sum, 0) + 1);
+        }
+        int[] rest;
+        if (Arrays.stream(left).anyMatch((a) -> a == 0)) {
+            res[0] = diff;
+            rest = recoverArray(n - 1, left);
+        } else {
+            res[0] = -diff;
+            rest = recoverArray(n - 1, right);
+        }
+        System.arraycopy(rest, 0, res, 1, rest.length);
+        return res;
+    }
+
+    private int getSize(int len) {
+        int size = 0;
+        while (len % 2 == 0) {
+            size++;
+            len /= 2;
+        }
+        return size;
+    }
 
 
     private List<Integer> getSubsets(int[] arr) {
@@ -29,5 +73,6 @@ public class FindArrayGivenSubsetsSums {
         System.out.println(arr);
         arr.sort(Integer::compareTo);
         System.out.println(arr);
+        System.out.println(Arrays.toString(find.recoverArray(3, new int[]{-3,-2,-1,0,0,1,2,3})));
     }
 }
