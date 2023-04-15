8faa837412dd008b451f1887e55af1d70686e254
mishamolnar <michaellmuller423@gmail.com>
2023-04-15 09:57:52 +0300

    14.04

---
+++ dynamic/LongestPalindromicSubsequence.java
@@ -0,0 +1,20 @@
+package LeetCode.dynamic;
+
+public class LongestPalindromicSubsequence {
+    public int longestPalindromeSubseq(String s) {
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][len - 1];
+    }
+}
